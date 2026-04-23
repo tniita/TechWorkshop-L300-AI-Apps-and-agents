2225cb18573125bb856b448a0535331c54400973
Takuya Niita <takuya2230@gmail.com>
2026-04-23 04:30:43 +0000

    Update agent_initializer and CustomerLoyaltyAgentPrompt

--- src/app/agents/agent_initializer.py
+++ src/app/agents/agent_initializer.py
@@ -17,3 +17,17 @@ def initialize_agent(project_client : AIProjectClient, model : str, name : str, 
             )
         )
         print(f"Created {name} agent, ID: {agent.id}")
+        
+def initialize_agent(project_client : AIProjectClient, model : str, name : str, description : str, instructions : str, tools : List[Any]):
+    with project_client:
+        agent = project_client.agents.create_version(
+            agent_name=name,
+            description=description,
+            definition=PromptAgentDefinition(
+                model=model,
+                instructions=instructions,
+                tools=tools
+            )
+        )
+        print(f"Created {name} agent, ID: {agent.id}")
+
